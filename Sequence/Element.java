e347960f2876450e901f8685ed8ad44c25cfa99b
Mageplasm <jitesh_bhatia@yahoo.com>
2016-04-23 22:18:45 -0700

    Added Element.java

---
+++ Sequence/Element.java
@@ -0,0 +1,15 @@
+
+public class Element {
+	
+	//Abstract print method
+	public void Print(){
+	}
+
+	//Abstract flatten method
+	public Sequence flatten() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
